7c538a2971b2afe10524e683f16a1c6bdbca0683
Namrata Pandey <71514235+Namrata020@users.noreply.github.com>
2024-05-14 13:15:08 +0530

    Update Program.cs

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,7 +5,7 @@ namespace Task1
 {
     class Program
     {
-        static List<string> list = new List<string>();
+        static List<string> tasks = new List<string>();
 
         public static void Main(string[] args)
         {
@@ -20,94 +20,36 @@ namespace Task1
                 Console.WriteLine("3. Update Task");
                 Console.WriteLine("4. Delete Task");
                 Console.WriteLine("5. Exit");
+                Console.WriteLine();
                 Console.Write("Enter your choice: ");
 
-                //converts string input to int
+                // Read the user's input as a string & convert it to an integer and assign it to a variable 'choice'
                 if (!int.TryParse(Console.ReadLine(), out choice))
                 {
-                    Console.WriteLine("Invalid choice. Please enter a number."); //checks for valid choice number
+                    Console.WriteLine("Invalid choice. Please enter a number.");
                     continue; // Skip the rest of the loop iteration and start over
                 }
 
                 switch (choice)
                 {
                     case 1:
-
                         //Create task
-                        Console.WriteLine();
-                        Console.Write("Enter the task: ");
-                        string task = Console.ReadLine();
-                        list.Add(task);
-                        Console.WriteLine("Task added successfully.");
+                        CreateTask();
                         break;
 
                     case 2:
-
                         //Read all tasks
-                        Console.WriteLine();
-                        if (list.Count == 0) //checks whether task list is empty or not
-                        {
-                            Console.WriteLine("Task list is empty.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Task List:");
-                            for (int i = 0; i < list.Count; i++) //iterates and prints tasks from the list
-                            {
-                                Console.WriteLine($"{i + 1}. {list[i]}");
-                            }
-                        }
+                        ReadTasks();
                         break;
 
                     case 3:
-
                         //Update task
-                        Console.WriteLine();
-                        Console.WriteLine("Task List: ");
-                        
-                        for (int i = 0; i < list.Count; i++)
-                        {
-                            Console.WriteLine($"{i + 1}. {list[i]}");
-                        }
-
-                        if (list.Count == 0)
-                        {
-                            return;
-                        }
-
-                        Console.Write("Enter the task number to update: ");
-                        if (int.TryParse(Console.ReadLine(), out int index) && index >= 1 && index <= list.Count)
-                        {
-                            Console.Write("Enter the updated task: ");
-                            string updatedTask = Console.ReadLine();
-                            list[index - 1] = updatedTask;
-                            Console.WriteLine("Task updated successfully.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid task number.");
-                        }
+                        UpdateTask();
                         break;
 
                     case 4:
-
                         //Delete Task
-                        if (list.Count == 0)
-                        {
-                            return;
-                        }
-
-                        Console.WriteLine();
-                        Console.Write("Enter the task number to delete: ");
-                        if (int.TryParse(Console.ReadLine(), out int indx) && indx >= 1 && indx <= list.Count) 
-                        {
-                            list.RemoveAt(indx - 1);
-                            Console.WriteLine("Task deleted successfully.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid task number.");
-                        }
+                        DeleteTask();
                         break;
 
                     case 5:
@@ -115,16 +57,83 @@ namespace Task1
                         break;
 
                     default:
-
-                        Console.WriteLine();
-                        Console.WriteLine("Enter a valid choice from 1-5");
+                        Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
                         break;
                 }
 
             } while (!exit);
         }
 
+        public static void CreateTask()
+        {
+            Console.WriteLine();
+            Console.Write("Enter the task: ");
+            string task = Console.ReadLine();
+            tasks.Add(task);
+            Console.WriteLine("Task added successfully.");
+        }
 
-       
+        public static void ReadTasks()
+        {
+            if (tasks.Count == 0) //checks whether task list is empty or not
+                {
+                Console.WriteLine();
+                Console.WriteLine("Task list is empty.");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Task List:");
+                for (int i = 0; i < tasks.Count; i++) //iterates and prints tasks from the list
+                    {
+                    Console.WriteLine($"{i + 1}. {tasks[i]}");
+                }
+            }
+        }
+
+        public static void UpdateTask()
+        {
+            ReadTasks();
+            if (tasks.Count == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter the task number to update: ");
+            if (int.TryParse(Console.ReadLine(), out int index) && index >= 1 && index <= tasks.Count)
+            {
+                Console.WriteLine();
+                Console.Write("Enter the updated task: ");
+                string updatedTask = Console.ReadLine();
+                tasks[index - 1] = updatedTask;
+                Console.WriteLine("Task updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid task number.");
+            }
+        }
+
+        public static void DeleteTask()
+        {
+            ReadTasks();
+            if (tasks.Count == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter the task number to delete: ");
+            if (int.TryParse(Console.ReadLine(), out int index) && index >= 1 && index <= tasks.Count)
+            {
+                tasks.RemoveAt(index - 1);
+                Console.WriteLine("Task deleted successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid task number.");
+            }
+        }
     }
 }
