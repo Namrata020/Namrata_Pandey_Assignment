6883c4ee5033053c441d1efd92f7839e459cc6f8
Namrata Pandey <71514235+Namrata020@users.noreply.github.com>
2024-06-24 12:34:19 +0530

    Add files via upload

---
+++ EmployeeManagementSystem/Interface/IEmployeeBasicDetailsService.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagementSystem.DTO;
+using EmployeeManagementSystem.Entities;
+using static EmployeeManagementSystem.Entities.EmployeeBasicDetails;
+
+namespace EmployeeManagementSystem.Interface
+{
+    public interface IEmployeeBasicDetailsService
+    {
+        Task<EmployeeBasicDetailsDto> AddEmployeeBasicDetails(EmployeeBasicDetailsDto employeeBasicDetailsDto);
+        Task<EmployeeAdditionalDetailsDto> AddEmployeeAdditionalDetails(EmployeeAdditionalDetailsDto employeeAdditionalDetailsDto);
+        Task<List<EmployeeBasicDetailsDto>> GetAllEmployeeBasicDetails();
+        Task<List<EmployeeAdditionalDetailsDto>> GetAllEmployeeAdditionalDetails();
+        Task<EmployeeBasicDetailsDto> UpdateEmployeeBasicDetails(EmployeeBasicDetailsDto employeeBasicdetailsDto);
+        Task<string> DeleteEmployeeBasicDetails(string uid);
+        Task<EmployeeBasicDetailsDto> AddEmployeeBasicDetailsByMakepostRequest(EmployeeBasicDetailsDto employeeBasicDetailsDto);
+        Task<EmployeeFilterCriteria> GetAllEmployeesByPagination(EmployeeFilterCriteria employeeFilterCriteria);
+        Task<List<EmployeeBasicDetailsDto>> GetAllEmployeeBasicDetailsByRole(string role);
+    }
+}
